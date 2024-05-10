eb9b59e5e027356c3c903bfd9a7186df0a012dd4
Shorekk <oscar.kibber@gmail.com>
2024-05-10 19:19:49 +0200

    Oscar

---
+++ src/Oscar/src/Oscar/Oscar.java
@@ -0,0 +1,18 @@
+package Oscar;
+
+public class Oscar {
+	    
+
+	public void imprimirNombre() {
+	        String nombre = "Oscar"; 
+	        System.out.println("Mi nombre es: " + nombre);
+	    }
+
+	    
+	public static void main(String[] args) {
+	        
+	        Oscar nombre = new Oscar();
+	        
+	        nombre.imprimirNombre();
+	    }
+	}
